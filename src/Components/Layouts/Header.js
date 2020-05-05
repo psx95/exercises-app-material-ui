91cdddac73a2f7c329b2cb6a7061376b7a12c847
psx95 <pranav.ps95@hotmail.com>
2020-05-04 22:33:52 -0400

    feat(peoject): adds initial material styling to the header & footer

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 export default props =>
-    <div>
-        <h1>Hello from Header</h1>
-    </div>
+    <AppBar position="static">
+        <Toolbar>
+            <Typography variant="headline" color="inherit">
+                Exercises Database
+            </Typography>
+        </Toolbar>
+    </AppBar>
