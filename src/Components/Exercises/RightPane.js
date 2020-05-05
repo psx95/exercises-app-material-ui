54ffbdea4624880311fafe1f8a1d54da59cbd48a
psx95 <pranav.ps95@hotmail.com>
2020-05-05 18:34:49 -0400

    feat(right-pane): completes the right pane and equalises height for both panes

--- src/Components/Exercises/RightPane.js
+++ src/Components/Exercises/RightPane.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Paper } from '@material-ui/core';
+import { Paper, Typography } from '@material-ui/core';
 
 export default props =>
     <Paper style={props.styles}>
-        Right Pane
+        <Typography
+            variant="h5">
+            Welcome!
+        </Typography>
+        <Typography
+            variant="body1"
+            style={{ marginTop: 20 }}>
+            Please select an execise from the list on the left
+        </Typography>
     </Paper>
