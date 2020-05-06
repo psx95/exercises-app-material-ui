c65a21a951749636807346128105a2e79ae4dfc8
psx95 <pranav.ps95@hotmail.com>
2020-05-05 21:32:25 -0400

    refactor(right-pane): removes log line

--- src/Components/Exercises/RightPane.js
+++ src/Components/Exercises/RightPane.js
@@ -4,7 +4,6 @@ import { Paper, Typography } from '@material-ui/core';
 export default ({ styles, exercise }) => {
     const title = exercise ? exercise.title : "Welcome!";
     const description = exercise ? exercise.description : "Please select an execise from the list on the left";
-    console.log("exercise is  " + exercise + "-----" + title + " +  " + description);
 
     return <Paper style={styles}>
         <Typography
