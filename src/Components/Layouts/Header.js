099384211b6fd968eb2d6a146347fa0d54068d23
psx95 <pranav.ps95@hotmail.com>
2020-05-08 01:18:35 -0400

    refactor(header): removes the use of inline styling

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import CreateDialog from '../Exercises/CreateDialog';
+import { withStyles } from '@material-ui/core/styles';
 
-export default ({ muscles, createExercise }) =>
+const styles = {
+    Flex: {
+        flex: 1
+    }
+}
+
+export default withStyles(styles)(({ classes, muscles, createExercise }) =>
     <AppBar position="static">
         <Toolbar>
-            <Typography variant="body1" color="inherit" style={{ flex: 1 }}>
+            <Typography variant="body1" color="inherit" className={classes.Flex}>
                 Exercises Database
             </Typography>
 
             <CreateDialog categories={muscles} onExerciseCreate={createExercise} />
         </Toolbar>
     </AppBar>
+)
